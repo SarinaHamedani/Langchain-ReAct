1b700ec3b741238f5b43d08137795090224f8565
SarinaHmd <sarina.hamedani@legisimple.ca>
2025-05-29 10:12:52 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from langchain.agents import tool
+
+load_dotenv()
+
+@tool
+def get_text_length(text:str) -> int:
+    """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip('"') # stripping away non alphabetic characters in case
+    return len(text)
+
+if __name__=="__main__":
+    print("Hello ReAct Langchain!")
+    print(get_text_length("Dog"))
